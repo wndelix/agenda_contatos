0b9f7211dcb76796784ee3498d1bb20aca922c37
Wendel <wende08@proton.me>
2026-08-31 21:08:54 -0300

    added v0.3.0 update

--- src/br/edu/principal/Principal.java
+++ src/br/edu/principal/Principal.java
@@ -9,18 +9,19 @@ public class Principal {
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
 		boolean userContinue = true, contactFound = false;
-		int option, indexToDelete = -1;
-		String contactNameSearch = "";
+		int option, contactIndex = -1;
+		String newContactName = "", newContactNumber = "", newContactEmail = "", contactNameSearch = "";
 		List<String> contactsName = new ArrayList<>(), contactsNumber = new ArrayList<>(), contactsEmail = new ArrayList<>();
 
-		System.out.print("Agenda de Contatos v.0.2.0");
+		System.out.print("Agenda de Contatos v.0.3.0");
 
 		while(userContinue) {
 			System.out.println("\n1 - Adicionar Contato");
 			System.out.println("2 - Listar Contato");
 			System.out.println("3 - Procurar Contato");
-			System.out.println("4 - Excluir Contato");
-			System.out.println("5 - Sair\n");
+			System.out.println("4 - Alterar Contato");
+			System.out.println("5 - Excluir Contato");
+			System.out.println("6 - Sair\n");
 
 			System.out.print("Selecione a opção desejada: ");
 			option = userInput.nextInt();
@@ -85,7 +86,40 @@ public class Principal {
 					}
 				}
 
-				case 4  -> {
+				case 4 -> {
+					System.out.print("Digite o nome que deseja procurar: ");
+					contactNameSearch = userInput.nextLine();
+					contactIndex = -1;
+
+					for (int i = 0; i < contactsName.size(); i++) {
+						if (contactsName.get(i).equalsIgnoreCase(contactNameSearch)) {
+							contactIndex = i;
+							break;
+						}
+					}
+
+					if (contactIndex != -1) {
+						System.out.print("Digite o novo nome: ");
+						newContactName = userInput.nextLine();
+
+						System.out.print("Digite o novo celular: ");
+						newContactNumber = userInput.nextLine();
+
+						System.out.print("Digite o novo email: ");
+						newContactEmail = userInput.nextLine();
+
+						contactsName.set(contactIndex, newContactName);
+						contactsNumber.set(contactIndex, newContactNumber);
+						contactsEmail.set(contactIndex, newContactEmail);
+
+						System.out.println("\nContato alterado com sucesso!");
+					} else {
+						System.out.println("Contato não encontrado!");
+					}
+				}
+
+
+				case 5  -> {
 					if (contactsName.isEmpty()) {
 						System.out.println("Nenhum contato cadastrado.\n");
 					}
@@ -94,28 +128,28 @@ public class Principal {
 						System.out.print("Digite o nome do contato que deseja excluir: ");
 						contactNameSearch = userInput.nextLine();
 
-						indexToDelete = -1;
+						contactIndex = -1;
 
 						for(int i = 0; i < contactsName.size(); i++){
 							if(contactsName.get(i).equalsIgnoreCase(contactNameSearch)) {
-								indexToDelete = i;
+								contactIndex = i;
 							}
 						}
-						if(indexToDelete == -1) {
+						if(contactIndex == -1) {
 							System.out.println("Nenhum contato encontrado.\n");
 						}
 						else{
-							contactsName.remove(indexToDelete);
-							contactsNumber.remove(indexToDelete);
-							contactsEmail.remove(indexToDelete);
+							contactsName.remove(contactIndex);
+							contactsNumber.remove(contactIndex);
+							contactsEmail.remove(contactIndex);
 
 							System.out.println("Contato excluído com sucesso.");
 						}
 					}
 				}
 
-				case 5  -> {
-					System.out.println("Saíndo...");
+				case 6  -> {
+					System.out.println("Saindo...");
 					userContinue = false;
 				}
 
