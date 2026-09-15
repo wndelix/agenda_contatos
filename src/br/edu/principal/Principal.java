f360f3bfbbd495412c18a648327872f21f950d11
Wendel Peterson <wende08@proton.me>
2026-09-15 11:05:31 -0300

    updated functions in main class

--- src/br/edu/principal/Principal.java
+++ src/br/edu/principal/Principal.java
@@ -1,11 +1,8 @@
 package br.edu.principal;
-
 import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
 
 public class Principal {
-
 	public static void main(String[] args) {
 		List<String> nomesContatos = new ArrayList<>();
 		List<String> numerosContatos = new ArrayList<>();
@@ -15,19 +12,19 @@ public class Principal {
 		boolean userContinue = true;
 		int opcao;
 		
-		mostraIntro();
+		Utils.mostrarIntro();
 
 		while(userContinue) {
-			mostraMenu();
-			opcao = selecionaOpcao(userInput);
+			Utils.mostrarMenu();
+			opcao = Utils.selecionarOpcao(userInput);
 
 			switch(opcao) {
 				default -> System.out.println("Opção Inválida. Tente Novamente.");
-				case 1  -> adicionaContato(userInput, nomesContatos, numerosContatos, emailsContatos);
-				case 2  -> listarContato(nomesContatos, numerosContatos, emailsContatos);
-				case 3  -> procurarContato(userInput, nomesContatos, numerosContatos, emailsContatos);
-				case 4  -> alterarContato(userInput, nomesContatos, numerosContatos, emailsContatos);
-				case 5  -> excluirContato(userInput, nomesContatos, numerosContatos, emailsContatos);
+				case 1  -> Agenda.adicionaContato(userInput, nomesContatos, numerosContatos, emailsContatos);
+				case 2  -> Agenda.listarContato(nomesContatos, numerosContatos, emailsContatos);
+				case 3  -> Agenda.procurarContato(userInput, nomesContatos, numerosContatos, emailsContatos);
+				case 4  -> Agenda.alterarContato(userInput, nomesContatos, numerosContatos, emailsContatos);
+				case 5  -> Agenda.excluirContato(userInput, nomesContatos, numerosContatos, emailsContatos);
 				case 6  -> {
 					System.out.println("Saindo...");
 					userContinue = false;
